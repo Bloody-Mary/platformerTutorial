044f730689953ddef8ae212034924dbea5bb73fd
mary <marianelalieben@gmail.com>
2024-02-11 18:11:49 +0300

    feature:11.02.2024 18:11 constructor in the GameWindow class was created and jFrame variable was added.

--- src/main/java/PlatformerTutorial/GameWindow.java
+++ src/main/java/PlatformerTutorial/GameWindow.java
@@ -1,4 +1,14 @@
 package PlatformerTutorial;
 
-public class GameWindow {
+import javax.swing.*;
+
+public class GameWindow{
+    private JFrame jFrame;
+
+    public GameWindow() {
+
+        jFrame = new JFrame();
+        jFrame.setSize(400, 400);
+    }
+
 }
