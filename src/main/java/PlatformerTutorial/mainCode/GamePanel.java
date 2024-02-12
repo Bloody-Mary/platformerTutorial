f3cf7d36eb256a07b61dd793cffb34b01ff7cee4
mary <marianelalieben@gmail.com>
2024-02-12 22:07:56 +0300

    feature:12.02.2024 22:07 new KeyboardInputs was added to constructor.

--- src/main/java/PlatformerTutorial/mainCode/GamePanel.java
+++ src/main/java/PlatformerTutorial/mainCode/GamePanel.java
@@ -1,30 +1,15 @@
 package PlatformerTutorial.mainCode;
 
+import PlatformerTutorial.inputs.KeyboardInputs;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel {
 
     public GamePanel() {
 
-        addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-
-            }
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-
-            }
-        });
+        addKeyListener(new KeyboardInputs());
     }
 
     public void paintComponent(Graphics g) {
