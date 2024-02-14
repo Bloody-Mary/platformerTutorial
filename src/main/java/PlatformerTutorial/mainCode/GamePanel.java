00f61eb5949265afca5799f545399fcef1dc1faa
mary <marianelalieben@gmail.com>
2024-02-14 17:50:57 +0300

    feature:14.02.2024 17:50 FPS counter was created.

--- src/main/java/PlatformerTutorial/mainCode/GamePanel.java
+++ src/main/java/PlatformerTutorial/mainCode/GamePanel.java
@@ -11,6 +11,8 @@ public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
     private int xDelta = 100;
     private int yDelta = 100;
+    private int frames = 0;
+    private long lastCheck = 0;
     public GamePanel() {
 
         mouseInputs = new MouseInputs(this);
@@ -35,6 +37,12 @@ public class GamePanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.fillRect(xDelta, yDelta, 200, 50);
+        frames++;
+        if (System.currentTimeMillis() - lastCheck >= 1000) {
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frames);
+            frames = 0;
+        }
         repaint();
     }
 }
