15edb989678b99858154ba856ff9317442e878f6
mary <marianelalieben@gmail.com>
2024-02-14 15:45:09 +0300

    feature:14.02.2024 15:45 methods changeXDelta and changeYDelta were created.

--- src/main/java/PlatformerTutorial/mainCode/GamePanel.java
+++ src/main/java/PlatformerTutorial/mainCode/GamePanel.java
@@ -14,14 +14,21 @@ public class GamePanel extends JPanel {
     public GamePanel() {
 
         mouseInputs = new MouseInputs();
-        addKeyListener(new KeyboardInputs());
+        addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
     }
 
+    public void changeXDelta(int value) {
+        this.xDelta += value;
+    }
+
+    public void changeYDelta(int value) {
+        this.yDelta += value;
+    }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
         g.fillRect(100 + xDelta, 100 + yDelta, 200, 50);
     }
 }
