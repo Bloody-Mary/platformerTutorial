333d40ca7f54f78f260cbebd265dcc34a746a4d3
mary <marianelalieben@gmail.com>
2024-02-14 16:58:00 +0300

    feature:14.02.2024 16:57 constructor and private variable were added.

--- src/main/java/PlatformerTutorial/inputs/MouseInputs.java
+++ src/main/java/PlatformerTutorial/inputs/MouseInputs.java
@@ -1,10 +1,18 @@
 package PlatformerTutorial.inputs;
 
+import PlatformerTutorial.mainCode.GamePanel;
+
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 public class MouseInputs implements MouseListener, MouseMotionListener {
+
+    private GamePanel gamePanel;
+
+    public MouseInputs(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
     @Override
     public void mouseClicked(MouseEvent e) {
         System.out.println("Mouse clicked!");
