41a810936cbe95fe5e4f8f52f527240e54c4edf3
mary <marianelalieben@gmail.com>
2024-02-14 15:41:30 +0300

    feature:14.02.2024 15:41 constructor was added to class KeyboardInputs.

--- src/main/java/PlatformerTutorial/inputs/KeyboardInputs.java
+++ src/main/java/PlatformerTutorial/inputs/KeyboardInputs.java
@@ -1,9 +1,16 @@
 package PlatformerTutorial.inputs;
 
+import PlatformerTutorial.mainCode.GamePanel;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyboardInputs implements KeyListener {
+
+    private GamePanel gamePanel;
+    public KeyboardInputs(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
     @Override
     public void keyTyped(KeyEvent e) {
 
