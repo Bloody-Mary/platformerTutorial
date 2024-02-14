e6ba1fa19d714b709901b18a283bf9253ddea52b
mary <marianelalieben@gmail.com>
2024-02-14 17:04:30 +0300

    fix:14.02.2024 17:04 method mouseMoved was changed.

--- src/main/java/PlatformerTutorial/inputs/MouseInputs.java
+++ src/main/java/PlatformerTutorial/inputs/MouseInputs.java
@@ -45,6 +45,6 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        System.out.println("Mouse moved!");
+        gamePanel.setRectPos(e.getX(), e.getY());
     }
 }
