35deafacd80181b9255cd0aba6a66d2dd4bc8e70
mary <marianelalieben@gmail.com>
2024-02-15 15:48:09 +0300

    fix:15.02.2024 15:48 variables gameThread and FPS_SET were added.

--- src/main/java/PlatformerTutorial/mainCode/Game.java
+++ src/main/java/PlatformerTutorial/mainCode/Game.java
@@ -1,9 +1,11 @@
 package PlatformerTutorial.mainCode;
 
-public class Game {
+public class Game implements Runnable{
 
     private GameWindow gameWindow;
     private GamePanel gamePanel;
+    private Thread gameThread;
+    private final int FPS_SET = 120;
     public Game() {
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
