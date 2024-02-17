b78b6bb6ea3301b1cc119f0bfb0e40c74a91abc4
mary <marianelalieben@gmail.com>
2024-02-17 19:05:08 +0300

    feature:17.02.2024 19:05 unusable methods and variables were deleted.

--- src/main/java/PlatformerTutorial/mainCode/GamePanel.java
+++ src/main/java/PlatformerTutorial/mainCode/GamePanel.java
@@ -5,20 +5,13 @@ import PlatformerTutorial.inputs.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Random;
 
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
     private float xDelta = 100, yDelta = 100;
-    private float xDir = 1f, yDir = 1f;
-    private int frames = 0;
-    private long lastCheck = 0;
-    private Color color = new Color(150, 20, 85);
-    private Random random;
 
     public GamePanel() {
-        random = new Random();
         mouseInputs = new MouseInputs(this);
         setPanelSize();
         addKeyListener(new KeyboardInputs(this));
@@ -46,29 +39,8 @@ public class GamePanel extends JPanel {
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        updateRectangle();
-        g.setColor(color);
-        g.fillRect((int) xDelta, (int) yDelta, 200, 50);
+
     }
 
-    private void updateRectangle() {
-        xDelta += xDir;
-        if (xDelta > 400 || xDelta < 0) {
-            xDir *= -1;
-            color = getRandColor();
-        }
-        yDelta+= yDir;
-        if (yDelta > 400 || yDelta < 0) {
-            yDir *= -1;
-            color = getRandColor();
-        }
-    }
-
-    private Color getRandColor() {
-        int red = random.nextInt(255);
-        int green = random.nextInt(255);
-        int blue = random.nextInt(255);
-        return new Color(red, green, blue);
-    }
 
 }
