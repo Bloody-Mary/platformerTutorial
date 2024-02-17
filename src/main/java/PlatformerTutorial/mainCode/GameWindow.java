1427387a7df6953e7099accc8243c224bfc158c2
mary <marianelalieben@gmail.com>
2024-02-17 18:48:43 +0300

    feature:17.02.2024 18:48 method setSize was deleted.

--- src/main/java/PlatformerTutorial/mainCode/GameWindow.java
+++ src/main/java/PlatformerTutorial/mainCode/GameWindow.java
@@ -8,7 +8,7 @@ public class GameWindow{
     public GameWindow(GamePanel gamePanel) {
 
         jFrame = new JFrame();
-        jFrame.setSize(400, 400);
+
         jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
         jFrame.setLocationRelativeTo(null);
