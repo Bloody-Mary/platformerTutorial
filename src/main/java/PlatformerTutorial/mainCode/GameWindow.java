c020d49c5aacd61cc6cc676419ff5597891d6c5a
mary <marianelalieben@gmail.com>
2024-02-17 19:00:59 +0300

    feature:17.02.2024 19:00 methods jFrame.setResizable and jFrame.pack were added.

--- src/main/java/PlatformerTutorial/mainCode/GameWindow.java
+++ src/main/java/PlatformerTutorial/mainCode/GameWindow.java
@@ -8,10 +8,11 @@ public class GameWindow{
     public GameWindow(GamePanel gamePanel) {
 
         jFrame = new JFrame();
-
         jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
         jFrame.add(gamePanel);
         jFrame.setLocationRelativeTo(null);
+        jFrame.setResizable(false);
+        jFrame.pack();
         jFrame.setVisible(true);
     }
 
