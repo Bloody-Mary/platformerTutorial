b00fe45bf67ca88f6f9c9a0eb7da562602ec3571
mary <marianelalieben@gmail.com>
2024-02-18 23:59:48 +0300

    feature:18.02.2024 23:59 method importImage was created.

--- src/main/java/PlatformerTutorial/mainCode/GamePanel.java
+++ src/main/java/PlatformerTutorial/mainCode/GamePanel.java
@@ -3,20 +3,36 @@ package PlatformerTutorial.mainCode;
 import PlatformerTutorial.inputs.KeyboardInputs;
 import PlatformerTutorial.inputs.MouseInputs;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
     private float xDelta = 100, yDelta = 100;
+    private BufferedImage img;
 
     public GamePanel() {
         mouseInputs = new MouseInputs(this);
+
+        importImage();
         setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+    }
+
+    private void importImage() {
+        InputStream inputStream = getClass().getResourceAsStream("/player_sprites.png");
+        try {
+            img = ImageIO.read(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void setPanelSize() {
@@ -40,6 +56,7 @@ public class GamePanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        g.drawImage(img, 0, 0, null);
     }
 
 
