41fca1d5adfcde392cf0d4bf8734fb6fc4cc9c11
mary <marianelalieben@gmail.com>
2024-03-27 22:59:55 +0300

    feature:27.03.2024 22:59 method GetLevelData was created.

--- src/main/java/PlatformerTutorial/utils/LoadSave.java
+++ src/main/java/PlatformerTutorial/utils/LoadSave.java
@@ -1,6 +1,9 @@
 package PlatformerTutorial.utils;
 
+import PlatformerTutorial.mainCode.Game;
+
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,6 +11,7 @@ import java.io.InputStream;
 public class LoadSave {
     public static final String PLAYER_ATLAS = "player_sprites.png";
     public static final String LEVEL_ATLAS = "outside_sprites.png";
+    public static final String LEVEL_ONE_DATA = "level_one_data.png";
     public static BufferedImage GetSpriteAtlas(String fileName) {
         BufferedImage img = null;
         InputStream inputStream = LoadSave.class.getResourceAsStream("/" + fileName);
@@ -23,5 +27,19 @@ public class LoadSave {
             }
         }
         return img;
+    }
+
+    public static int[][] GetLevelData() {
+        int[][] lvlData = new int[Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
+        BufferedImage image = GetSpriteAtlas(LEVEL_ONE_DATA);
+        for(int j = 0; j < image.getHeight(); j++)
+            for(int i = 0; i < image.getWidth(); i++) {
+                Color color = new Color(image.getRGB(i, j));
+                int value = color.getRed();
+                if (value >= 48)
+                    value = 0;
+                lvlData[i][j] = value;
+            }
+        return lvlData;
     }
 }
